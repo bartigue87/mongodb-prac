bf75e6a24999bd2313bcb2e88b0ec52a60ae1362
Brandon Artigue <bartigue87@yahoo.com>
2022-06-11 16:13:34 -0600

    first commit

---
+++ mongo.js
@@ -0,0 +1,29 @@
+const MongoClient = require("mongodb").MongoClient;
+const url =
+  "mongodb+srv://bartigue:mMWJwLPGPn3Dp38r@cluster1.hvwob4u.mongodb.net/products?retryWrites=true&w=majority";
+
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = db.collection("products").insertOne(newProduct);
+  } catch (error) {
+    return res.json({ message: "Could not store data" });
+  }
+  setTimeout(() => {
+    client.close();
+  }, 1500);
+  res.json(newProduct);
+};
+
+const getProduct = async (req, res, next) => {};
+
+exports.createProduct = createProduct;
+exports.getProduct = getProduct;
